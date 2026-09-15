723dcb39520b74deb9dcf58ad7a7d2eb89989118
yassine-ED <yassine.eddaadoua37@gmail.com>
2026-09-15 16:51:19 +0100

    feat: initial commit with CI/CD workflows

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /', () => {
+  it('should return 200 OK', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello CI/CD World!');
+  });
+});
